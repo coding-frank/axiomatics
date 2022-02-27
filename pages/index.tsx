692489d52c37d596a8fd0e010bb36bc1bc0395ec
Frank Arlt <arlt1979@gmail.com>
2022-02-27 12:55:31 +0100

    Added string replacement function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import XMLParser from "xml-parser-xo";
+import uuid from "react-uuid";
 
 function Main() {
 	const [selectedFile, setSelectedFile] = useState<Blob | null>(null);
@@ -15,6 +16,16 @@ function Main() {
 		inportFile();
 	};
 
+	// crop \r \n \t from text
+	const replaceLineFeeds = (string: string) => {
+		console.log("string: ", string);
+		let newString = string.replace(/\r/g, "");
+		newString = newString.replace(/\n/g, "");
+		newString = newString.replace(/\t/g, "");
+
+		return newString;
+	};
+
 	const inportFile = () => {
 		// init FileReader to import file data
 		const reader = new FileReader();
@@ -23,16 +34,19 @@ function Main() {
 			const text: string | ArrayBuffer = reader.result;
 
 			const xmlParsed = XMLParser(text);
-			console.log(xmlParsed);
+
+			// take children from xml file only
+			const children = xmlParsed.children;
 
 			// set parsed xml to state
-			setData(xmlParsed);
+			setData(children);
 		};
 
 		reader.onload = onload;
 		reader.readAsText(selectedFile);
 	};
 
+	console.log("data", data);
 	const handleChange = (e) => {
 		// check for file
 		if (e.target.files.length === 0) return;
@@ -48,6 +62,55 @@ function Main() {
 		setError(null);
 	};
 
+	const ShowList = ({ subitems, name }) => {
+		return (
+			<li key={uuid()}>
+				{name}
+				<ul className='tree-list'>
+					{subitems
+						.filter((sub) => sub.content !== "")
+						.map((item) => {
+							if (item.children) {
+								return <ShowList key={uuid()} subitems={item.children} name={item.name} />;
+							}
+
+							if (!item.children) {
+								return <ShowListItem key={uuid()} item={item} />;
+							}
+						})}
+				</ul>
+			</li>
+		);
+	};
+
+	const ShowListItem = ({ item }) => {
+		if (item.type === "Element") return <li>{item.name}</li>;
+		if (item.type === "Text") {
+			const clearedContent = replaceLineFeeds(item.content);
+
+			return clearedContent.length > 0 ? <li>{clearedContent}</li> : null;
+		}
+
+		return null;
+	};
+
+	const ListingComponent = () => {
+		const outputArr = [];
+
+		// loop though data and build output array
+		for (let i = 0; i < data.length; i++) {
+			if (data[0].children) {
+				outputArr.push(<ShowList key={uuid()} subitems={data[0].children} name={data[0].name} />);
+			}
+
+			if (!data[0].children) {
+				outputArr.push(<ShowListItem key={uuid()} item={data[0]} />);
+			}
+		}
+
+		return <ul className='tree-list'>{outputArr}</ul>;
+	};
+
 	return (
 		<>
 			<h1>XML Upload Demo</h1>
@@ -55,7 +118,7 @@ function Main() {
 			{!data && (
 				<form>
 					<div className='search'>
-						<input type='file' name='example' accept='.xml' onChange={handleChange} />
+						<input type='file' data-testid='input-field' name='example' accept='.xml' onChange={handleChange} />
 						<button
 							type='button'
 							className='btn btn--blue'
@@ -70,7 +133,7 @@ function Main() {
 				</form>
 			)}
 
-			{data && <section>show</section>}
+			{data && <ListingComponent />}
 		</>
 	);
 }
