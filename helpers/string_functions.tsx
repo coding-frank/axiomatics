a8a0eb26f71f9f3582e9bbf885e2df5f4c4ceb42
Frank Arlt <arlt1979@gmail.com>
2022-02-28 12:38:12 +0100

    updated typo

--- helpers/string_functions.tsx
+++ helpers/string_functions.tsx
@@ -1,5 +1,5 @@
 // crop \r \n \t from text
-export const replaceLineFeeds = (string: string) => {
+export const replaceLineFeeds = (string: string): string => {
 	let newString = string.replace(/\r/g, "");
 	newString = newString.replace(/\n/g, "");
 	newString = newString.replace(/\t/g, "");
